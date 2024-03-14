fa746a8358e9020d3d4236c6b968007fa6135ea8
192anchal <120078392+192anchal@users.noreply.github.com>
2024-03-14 11:39:58 +0530

    Time: 117 ms (83.33%) | Memory: 50.3 MB (52.56%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int NumSubarraysWithSum(int[] nums, int goal) {
+        var prefixSum = 0;
+        var result = 0;
+        var count = new int[nums.Length + 1];
+        count[0] = 1;
+        foreach (var num in nums)
+        {
+            prefixSum += num;
+            if (prefixSum >= goal)
+            {
+                result += count[prefixSum - goal];
+            }
+            count[prefixSum]++;
+        }
+        return result;
+    }
+}
